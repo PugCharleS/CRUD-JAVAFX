9f0595b734215428d8b7cb04e12f17f1be8172be
Carlos Solis <e-cmsolis@neoris.com>
2021-07-28 22:19:06 -0500

    Version 1.2 (Eliminar, Cambio de Vistas con Menu e imagen agregados)

---
+++ src/main/java/Controlador/InsercionCompanyController.java
@@ -0,0 +1,135 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+import Modelo.AddressCompany;
+import Modelo.CEOCompany;
+import Modelo.Company;
+import Modelo.Peticiones;
+import static Modelo.Peticiones.doPostRequest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.response.Response;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javax.swing.JOptionPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author charl
+ */
+public class InsercionCompanyController implements Initializable {
+
+    @FXML
+    private TextField txtNombre;
+    @FXML
+    private TextField txtTel;
+    @FXML
+    private TextField txtEstado;
+    @FXML
+    private TextField txtCiudad;
+    @FXML
+    private TextField txtCalle;
+    @FXML
+    private TextField txtCP;
+    @FXML
+    private TextField txtNum;
+    @FXML
+    private TextField txtNombreCeo;
+    @FXML
+    private TextField txtTelCeo;
+    @FXML
+    private Button btnCargar;
+    @FXML
+    private Button btnLimpiar;
+    @FXML
+    private Button btnInicio;
+    
+    PrincipalControlador principal;
+    
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    public void PrincipalControlador(PrincipalControlador principal){
+        this.principal = principal;
+    }
+
+    @FXML
+    private void EventosInsercion(ActionEvent event) {
+        Object evt = event.getSource();
+        
+        if (evt.equals(btnCargar)) {
+            if (!txtNombre.getText().isEmpty() && !txtTel.getText().isEmpty() && !txtEstado.getText().isEmpty() && !txtCiudad.getText().isEmpty() && !txtCalle.getText().isEmpty() && !txtCP.getText().isEmpty() && !txtNum.getText().isEmpty() && !txtNombreCeo.getText().isEmpty() && !txtTelCeo.getText().isEmpty()) {
+                Company company = new Company();
+                company.setName(txtNombre.getText());
+                company.setPhone(txtTel.getText());
+
+                AddressCompany address = new AddressCompany();
+                address.setCity(txtCiudad.getText());
+                address.setState(txtEstado.getText());
+                address.setNumber(Integer.parseInt(txtNum.getText()));
+                address.setStreet(txtCalle.getText());
+                address.setZipcode(txtCP.getText());
+
+                CEOCompany ceo = new CEOCompany();
+                ceo.setName(txtNombreCeo.getText());
+                ceo.setPhone(txtTelCeo.getText());
+
+                company.setAddressCompany(address);
+                company.setCeoCompany(ceo);
+
+
+                ObjectMapper mapper = new ObjectMapper();
+                String json;
+                 try {
+                     json = mapper.writeValueAsString(company);
+                     Response response = doPostRequest(json, "http://localhost:3000/company");
+                     if (response.getStatusCode() >= 200 && response.getStatusCode() <= 299) {
+                         JOptionPane.showMessageDialog(null, "Operacion Exitosa " + response.getStatusLine());
+                         LimpiarCampos();
+                     } else {
+                         System.out.println("Error: no hay respuesta");
+                     }
+                 } catch (JsonProcessingException ex) {
+                     Logger.getLogger(Peticiones.class.getName()).log(Level.SEVERE, null, ex);
+                 }
+            } else {
+                JOptionPane.showMessageDialog(null, "Los campos no están completos");
+            }
+        } else if (evt.equals(btnLimpiar)){
+            LimpiarCampos();
+        } else if (evt.equals(btnInicio)){
+            principal.CargarVista("Inicio");
+        }
+    }   
+    
+    public void LimpiarCampos(){
+            txtNombre.setText("");
+            txtTel.setText("");
+            txtCiudad.setText("");
+            txtEstado.setText("");
+            txtNum.setText("");
+            txtCalle.setText("");
+            txtCP.setText("");
+            txtNombreCeo.setText("");
+            txtTelCeo.setText("");
+    }
+    
+}
---
+++ src/main/java/Controlador/ListadoCompanyController.java
@@ -0,0 +1,168 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+import Modelo.Company;
+import Modelo.Peticiones;
+import static Modelo.Peticiones.doPostRequest;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.response.Response;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javax.swing.JOptionPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author charl
+ */
+public class ListadoCompanyController implements Initializable {
+
+    PrincipalControlador principal;
+    
+    @FXML private Button btnLista, btnInicio;
+    
+    @FXML private TableView<Company> tvLista;
+    private ObservableList<Company> data;
+    
+    private MenuItem miModificar, miEliminar;
+    
+    private ContextMenu cmOpciones; 
+    
+    public void PrincipalControlador(PrincipalControlador principal){
+        this.principal = principal;
+    }
+    
+    @FXML public void CargarLista(ActionEvent event){
+        Response res = Peticiones.doGetRequest("http://localhost:3000/company");
+        tvLista.getItems().removeAll(data);
+        ObjectMapper mapper = new ObjectMapper();
+//        System.out.println("\t __^__                          __^__\n" +
+//                           "\t( ___ )------------------------( ___ )\n" +
+//                           "\t | / |                          | / |\n" +
+//                           "\t | / |     Conexion Exitosa!    | / |\n" +
+//                           "\t |___|                          |___|\n" +
+//                           "\t(_____)------------------------(_____)");   
+        try {
+            ArrayList<Company> listCompanys  = mapper.readValue(res.asString(), new TypeReference<ArrayList<Company>>(){});
+            
+            if (listCompanys.size()>0) {
+                for (int i = 0; i < listCompanys.size(); i++) {
+//                    listCompanys.get(i).setCeo(listCompanys.get(i).getCeoCompany().toString());
+                    data.add(listCompanys.get(i));
+//                    System.out.println(data.get(i).getCeoCompany());                      
+                }             
+            }else{
+                System.out.println("Error: no hay informacion");
+            }             
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }       
+    }
+    
+    @FXML public void CargarInicio(ActionEvent event){
+        principal.CargarVista("Inicio");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        /*
+        Response res = Peticiones.doGetRequest("http://localhost:3000/company");
+        ObjectMapper mapper = new ObjectMapper();
+            
+        try {
+            ArrayList<Company> listCompanys  = mapper.readValue(res.asString(), new TypeReference<ArrayList<Company>>(){});
+            
+            if (listCompanys.size()>0) {
+                for (int i = 0; i < listCompanys.size(); i++) {
+                    System.out.println(listCompanys.get(i));
+                }                             
+            }else{
+                System.out.println("Error: no hay informacion");
+            }             
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }  
+        */
+        
+        cmOpciones = new ContextMenu();
+        miEliminar = new MenuItem("Eliminar");
+        miModificar = new MenuItem("Modificar");
+        cmOpciones.getItems().addAll(miEliminar, miModificar);
+        
+        
+        tvLista.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
+        //nombre de las columnas
+        TableColumn<Company, String> colId = new TableColumn<>("ID");
+        TableColumn<Company, String> colNombre = new TableColumn<>("Nombre");
+        TableColumn<Company, String> colDir = new TableColumn<>("Direccion");
+        TableColumn<Company, String> colTel = new TableColumn<>("Telefono");
+        TableColumn<Company, String> colCeo = new TableColumn<>("CEO");
+        
+        //relaciona el valor para cada columna       
+        colId.setCellValueFactory(new PropertyValueFactory<>("id"));  
+        colNombre.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colDir.setCellValueFactory(new PropertyValueFactory<>("addressCompany"));  
+        colTel.setCellValueFactory(new PropertyValueFactory<>("phone"));  
+        colCeo.setCellValueFactory(new PropertyValueFactory<>("ceoCompany"));  
+        
+        //añade columnas al tableview
+        tvLista.getColumns().addAll(colId); 
+        tvLista.getColumns().addAll(colNombre);         
+        tvLista.getColumns().addAll(colDir);  
+        tvLista.getColumns().addAll(colTel); 
+        tvLista.getColumns().addAll(colCeo); 
+        
+        //inicializamos datos
+        data = tvLista.getItems();  
+        
+        tvLista.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent> () {
+            @Override
+            public void handle(MouseEvent event) {
+                if (event.getButton() == MouseButton.SECONDARY) {
+                    cmOpciones.show(tvLista, event.getScreenX(), event.getScreenY());
+                }
+            }
+        });
+        
+        miEliminar.setOnAction((event) -> {
+            if(tvLista.getSelectionModel().getSelectedIndex() >= 0){
+                cmOpciones.hide();
+                int id = tvLista.getSelectionModel().getSelectedItem().getId();
+                Response response = Peticiones.doDeleteRequest("http://localhost:3000/company", id);
+                if (response.getStatusCode() >= 200 && response.getStatusCode() <= 299) {
+                         JOptionPane.showMessageDialog(null, "Operacion Exitosa " + response.getStatusLine());
+                         tvLista.getItems().remove(tvLista.getSelectionModel().getSelectedIndex());
+                         tvLista.refresh();
+                     } else {
+                         System.out.println("Error: no hay respuesta");
+                     }
+            }
+        });
+        
+        miModificar.setOnAction((event) -> {
+            
+        });
+        
+    }   
+    
+}
